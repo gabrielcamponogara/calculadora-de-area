818a48bd9a42efd9d3848549bab21ef5afe3afbe
gabrielcamponogara <seu@email.com>
2026-05-06 15:08:02 -0300

    primeiro commit

---
+++ src/main/java/principal/ProjetoTriangulo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package principal;
+import visao.FrmTriangulo;
+/**
+ *
+ * @author RTX4060
+ */
+public class ProjetoTriangulo {
+
+    public static void main(String[] args) {
+        FrmTriangulo janela = new FrmTriangulo();
+        janela.setVisible(true);
+    }
+}
